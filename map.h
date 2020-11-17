1ee60b0ebd7e052d6148fdf6f867889ac1089fe2
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2020-11-17 23:11:59 +0200

    Update map.h

--- map.h
+++ map.h
@@ -12,7 +12,7 @@ enum class TYPE {BOT,BLOCK,CRASH_BLOCK,CORRIDOR,BOMB,EXPLOSION};
 
 
 
-class Board : public QAbstractListModel
+class Map : public QAbstractListModel
 {
     Q_OBJECT
     //Q_PROPERTY()
@@ -20,12 +20,13 @@ public:
 
     static constexpr size_t defaultBordSize{49};
 
-    Board( const size_t bordSize = defaultBordSize, QObject* parent = nullptr );
+    Map( const size_t bordSize = defaultBordSize, QObject* parent = nullptr );
 
     struct Piece{
+        Piece(){};
         Piece(const char& sumbolG,const TYPE &typeG):sumbol_(sumbolG),type_(typeG){};
-        char sumbol_{};
-        TYPE type_{};
+        char sumbol_;
+        TYPE type_;
         bool operator ==(const TYPE otherType){
             return type_ == otherType;
         }
