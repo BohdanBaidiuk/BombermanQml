ee2893b1ac6081a71547f78f046129b2b001afaa
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2020-11-18 22:36:08 +0200

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -2,12 +2,17 @@
 #include <algorithm>
 #include <QDebug>
 
+QString grass = "image/grass.jpg";
+QString brick_wall ="image/brick_wall.jpg";
+
 Map::Map(const size_t bordSize, QObject *parent):QAbstractListModel(parent), m_boardSize(bordSize)
 {
-    m_boardMap;
-    m_boardMap.resize(m_boardSize);
+    //m_boardMap.resize(4);
     fillBoard();
 }
+
+
+
 
 int Map::rowCount(const QModelIndex &parent) const
 {
@@ -17,7 +22,7 @@ int Map::rowCount(const QModelIndex &parent) const
 
 QVariant Map::data(const QModelIndex &index, int role) const
 {
-    if(!index.isValid() || role != Qt::DisplayRole){
+    if(!index.isValid()){
         return {};
     }
     const int rowIndex{index.row()};
@@ -25,63 +30,79 @@ QVariant Map::data(const QModelIndex &index, int role) const
     if(!ispositionValid(rowIndex)){
         return {};
     }
-    qDebug()<<rowIndex;
-    return QVariant::fromValue(m_boardMap[rowIndex].sumbol_);
+
+    switch (role) {
+    case Qt::DisplayRole:
+        return QVariant(QString(m_boardMap[rowIndex].sumbol));
+    case ImageRole:
+        return QVariant(m_boardMap[rowIndex].image);
+    }
+
+
+    return QVariant();
+}
+
+QHash<int, QByteArray> Map::roleNames() const
+{
+    QHash<int,QByteArray> roles;
+    roles[Qt::DisplayRole] = "display";
+    roles[ImageRole] = "image";
+    return roles;
+
 }
 
 void Map::fillBoard()
 {
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{' ',TYPE::CORRIDOR});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
-    m_boardMap.emplace_back(Piece{'#',TYPE::BLOCK});
+
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{' ',grass});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
+    m_boardMap.emplace_back(Piece{'#',brick_wall});
 
 }
 
